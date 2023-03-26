23367e590ee62420ca3e518572a0306ca557ed18
Damian Smakulski <damian.smakulski@me.com>
2023-03-26 11:22:12 +0200

    Create project for task

---
+++ Transactional-Key-Value-Store/Transactional_Key_Value_StoreApp.swift
@@ -0,0 +1,21 @@
+//
+//  Transactional_Key_Value_StoreApp.swift
+//  Transactional-Key-Value-Store
+//
+//  Created by Damian Smakulski on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Transactional_Key_Value_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+        #if os(macOS)
+            ContentView().frame(width: 500, height: 800)
+        #else
+            ContentView()
+        #endif
+        }
+    }
+}
